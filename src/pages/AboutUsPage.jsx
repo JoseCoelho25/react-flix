a2657abe9307828f1139a4a42fa5194ffeca0b35
JoseCoelho25 <zenani87@hotmail.com>
2023-01-14 17:27:28 +0000

    sync categoriesPage

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import code from "../images/code.png";
+import chibi from "../images/chibi.png";
+import mail from "../images/mail.png";
 import MainLayout from '../layouts/MainLayout'
 
 function AboutUsPage() {
   return (
     <MainLayout>
-      <div>AboutUsPage</div>
+      <div className="text-white container mx-auto mt-16 font-['Consolas'] mb-16">
+        <img src={code} alt="code" className="h-48" />
+        <div className=" text-4xl mb-10">This is a project i took from an exercise we made during my classes in FullStack Developer. The end goal is to create a netflix style site, using React library and tailwind for the CSS in this project.</div>
+        <div className="flex">
+          <div className="text-2xl mb-10 pr-10">My name is José Coelho, im from Porto, Portugal. Im currently finishing a FullStack degree at Flag scholl, and im open for job offers!</div>
+          <img src={chibi} alt="chibi" className="h-24 bg-white"/>
+        </div>
+        <div className="flex">
+          <img src={mail} alt="mail" className="" />
+          <div className="text-2xl pt-3 ml-5">If you need any information, or have any suggestions, you can contact me at <span className="text-red-700 underline decoration-red-700">zenani87@hotmail.com</span></div>
+        </div>
+      </div>
     </MainLayout>
   )
 }
--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MainLayout from '../layouts/MainLayout'
+import data from "../data/movies.json";
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function CategoriesPage() {
+  //Created a clone of JSON object
+  const clone = JSON.parse(JSON.stringify(data))
+
+  const [value, setValue] = useState([]);
+  const [orderByLikes, setOrderByLikes] = useState([]);
+  const array = data.sort((a,b) => a.score > b.score ? -1 : 1);
+  const orderLikesArray = clone.sort((a,b)=> a.likes > b.likes ? -1 : 1);
+
+  useEffect(() => {
+    setValue(array.slice(0,7));
+    setOrderByLikes(orderLikesArray.slice(0,7));
+  }, []);
+ 
   return (
     <MainLayout>
-      <div>SeriesPage</div>
+      <div className="text-white container mx-auto text-4xl my-10">Highest ranking films!</div>
+      <div className="grid grid-cols-7 grid-rows-1 gap-y-20 gap-x-16 bg-black text-white container mx-auto">
+      {value.length === 0 ? <div>Loading...</div> : 
+        value.map((movie) => (
+            <Link to={`/${movie.slug}`} key={movie.title} >
+              <img src={movie.imageUrl} alt="card" 
+              className="h-full w-full"
+              />
+              <div className="text-center">{movie.title}</div>
+            </Link>
+        ))
+      }
+      </div>
+      <div className="text-white container mx-auto text-4xl my-10">Most liked films!</div>
+        <div className="grid grid-cols-7 grid-rows-1 gap-y-20 gap-x-16 bg-black text-white container mx-auto">
+      {orderByLikes.length === 0 ? <div>Loading...</div> : 
+        orderByLikes.map((movie) => (
+            <Link to={`/${movie.slug}`} key={movie.title} >
+              <img src={movie.imageUrl} alt="card" 
+              className="h-full w-full"
+              />
+              <div className="text-center">{movie.title}</div>
+            </Link>
+        ))
+      }
+      </div>
     </MainLayout>
   )
 }
