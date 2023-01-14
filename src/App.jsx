d98ecb416f1dc89da6db14cad756561f2635d56f
JoseCoelho25 <zenani87@hotmail.com>
2023-01-14 21:41:44 +0000

    sync categories page and functionnality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {useLocation, BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from './pages/HomePage';
 import CategoriesPage from './pages/CategoriesPage';
 import AboutUsPage from './pages/AboutUsPage';
 import ContactUsPage from './pages/ContactUsPage';
 import SingleMoviePage from './pages/SingleMoviePage';
+import { useEffect } from 'react';
 
 function App() {
+
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+  
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+  
+    return null;
+  }
+
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />}/>
         <Route path="/categories" element={<CategoriesPage />}/>
--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -6,40 +6,41 @@ import { useEffect } from 'react';
 
 function CategoriesPage() {
   //Created a clone of JSON object
-  const clone = JSON.parse(JSON.stringify(data))
+  const cloneForLikes = JSON.parse(JSON.stringify(data))
+  const cloneForScore = JSON.parse(JSON.stringify(data))
 
   const [value, setValue] = useState([]);
   const [orderByLikes, setOrderByLikes] = useState([]);
-  const array = data.sort((a,b) => a.score > b.score ? -1 : 1);
-  const orderLikesArray = clone.sort((a,b)=> a.likes > b.likes ? -1 : 1);
+  const array = cloneForScore.sort((a,b) => a.score > b.score ? -1 : 1);
+  const orderLikesArray = cloneForLikes.sort((a,b)=> a.likes > b.likes ? -1 : 1);
 
   useEffect(() => {
     setValue(array.slice(0,7));
-    setOrderByLikes(orderLikesArray.slice(0,7));
+    setOrderByLikes(orderLikesArray.slice(0,14));
   }, []);
  
   return (
     <MainLayout>
-      <div className="text-white container mx-auto text-4xl my-10">Highest ranking films!</div>
-      <div className="grid grid-cols-7 grid-rows-1 gap-y-20 gap-x-16 bg-black text-white container mx-auto">
+      <div className="text-white container mx-auto text-4xl my-10 ">Highest ranking films!</div>
+      <div className="grid grid-cols-7 grid-rows-1 gap-y-20 gap-x-4 bg-black text-white container mx-auto border-t-2 border-red-700">
       {value.length === 0 ? <div>Loading...</div> : 
         value.map((movie) => (
             <Link to={`/${movie.slug}`} key={movie.title} >
               <img src={movie.imageUrl} alt="card" 
-              className="h-full w-full"
+              className="h-full w-full mt-4"
               />
               <div className="text-center">{movie.title}</div>
             </Link>
         ))
       }
       </div>
-      <div className="text-white container mx-auto text-4xl my-10">Most liked films!</div>
-        <div className="grid grid-cols-7 grid-rows-1 gap-y-20 gap-x-16 bg-black text-white container mx-auto">
+      <div className="text-white container mx-auto text-4xl mt-20 mb-10">Most liked films!</div>
+        <div className="grid grid-cols-7 grid-rows-2 gap-y-20 gap-x-4 bg-black text-white container mx-auto mb-20 border-t-2 border-red-700">
       {orderByLikes.length === 0 ? <div>Loading...</div> : 
         orderByLikes.map((movie) => (
             <Link to={`/${movie.slug}`} key={movie.title} >
               <img src={movie.imageUrl} alt="card" 
-              className="h-full w-full"
+              className="h-full w-full mt-4"
               />
               <div className="text-center">{movie.title}</div>
             </Link>
